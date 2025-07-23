1f70f4bc0559f344da7531e91c8cfd47291551da
Jika <jiksboy2000@gmail.com>
2025-07-23 23:19:37 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <p className="bg-green-200">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
