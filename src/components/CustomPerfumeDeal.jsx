9b07bc4c8346262a436516a8487b1636bccdd648
Jika <jiksboy2000@gmail.com>
2025-09-13 15:34:48 +0100

    hero overlay, items length

--- src/components/CustomPerfumeDeal.jsx
+++ src/components/CustomPerfumeDeal.jsx
@@ -8,22 +8,26 @@ const CustomPerfumeDeal = ({ title, perfumes }) => {
   // let featuredPerfumes = perfumes; //change this with filtering through for thoose with cateofries featured
 
   return (
-    <section className="py-4 sm:py-8 md:py-12 px-6 mt-4">
-      <div className="max-w-7xl mx-auto">
-        <h2 className="text-xl sm:text-3xl font-medium font-display italic mb-4 sm:mb-6 md:mb-8">
-          {title}
-        </h2>
+    <>
+      {perfumes.length > 0 && (
+        <section className="py-4 sm:py-8 md:py-12 px-6 mt-4">
+          <div className="max-w-7xl mx-auto">
+            <h2 className="text-xl sm:text-3xl font-medium font-display italic mb-4 sm:mb-6 md:mb-8">
+              {title}
+            </h2>
 
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-2 gap-y-4 sm:gap-4 md:gap-8">
-          {perfumes.map((item) => (
-            <CustomItem
-              item={item}
-              key={item.id}
-            />
-          ))}
-        </div>
-      </div>
-    </section>
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-2 gap-y-4 sm:gap-4 md:gap-8">
+              {perfumes.slice(0, 4).map((item) => (
+                <CustomItem
+                  item={item}
+                  key={item.id}
+                />
+              ))}
+            </div>
+          </div>
+        </section>
+      )}
+    </>
   );
 };
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
-import heroBackground from "../assets/images/background/heroBackground.webp";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import heroBackground from '../assets/images/background/heroBackground.webp';
 
 const Hero = () => {
   return (
@@ -9,34 +9,36 @@ const Hero = () => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className="min-h-screen flex flex-col justify-center items-center text-center text-white"
+      className="relative min-h-screen flex flex-col justify-center items-center text-center text-white"
       style={{
         backgroundImage: `url(${heroBackground})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
-      <motion.h1
-        initial={{ y: 50, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ delay: 0.3, duration: 1, type: "spring" }}
-        className="text-4xl md:text-6xl xl:text-7xl font-bold font-display italic mb-12 max-w-xl"
-      >
-        Elevate everyday moments to extraordinary.
-      </motion.h1>
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}>
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-gradient-to-tr from-black/90 via-black/40 to-transparent"></div>
 
-      <motion.div
-        initial={{ y: 20, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ delay: 0.8, duration: 0.8, type: "spring" }}
-      >
-        <Link
-          to="/store"
-          className="bg-white text-[#9c6a24] px-6 py-3 rounded-full font-semibold font-manrope hover:bg-opacity-90 transition"
-        >
-          SHOP OUR FRAGRANCES
-        </Link>
-      </motion.div>
+      {/* Content */}
+      <div className="relative z-10">
+        <motion.h1
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ delay: 0.3, duration: 1, type: 'spring' }}
+          className="text-4xl md:text-6xl lg:text-7xl xl:text-[6rem] font-bold font-display italic mb-12 max-w-3xl">
+          Elevate everyday moments to extraordinary.
+        </motion.h1>
+
+        <motion.div
+          initial={{ y: 20, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ delay: 0.8, duration: 0.8, type: 'spring' }}>
+          <Link
+            to="/store"
+            className="bg-white text-[#9c6a24] px-6 py-3 rounded-full font-semibold font-manrope hover:bg-opacity-90 transition">
+            SHOP OUR FRAGRANCES
+          </Link>
+        </motion.div>
+      </div>
     </motion.div>
   );
 };
